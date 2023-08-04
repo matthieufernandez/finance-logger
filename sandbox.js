755a7ed78edcc3b14fc6b1bafbec2ab5054900d9
matt <mrmatthieufernandez@gmail.com>
2023-08-04 13:18:37 -0400

    added a class and construtor to create invoices

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,37 @@
-// const add = (a: number, b: number, c?: number | string) => {
-//     console.log(a + b);
-//     console.log(c);
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes $").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invoices = [];
+// const invOne = new Invoice("Yoshi", "plumbing", 250);
+// console.log(invOne.format());
+// Dealing with forms and inputs with TypeScript
+var form = document.querySelector(".new-item-form");
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var invoice = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    console.log(invoice.format());
+});
+// TypeCasting
+// let logDetails: (obj: {name: string, age: number}) => void;
+// type dog = {name: string, age: number};
+// logDetails = (goodBoy: dog) => {
+//     console.log(`His name is: ${goodBoy.name} and he is ${goodBoy.age} years old.`);
 // }
-// const minus = (a: number, b:number): number => {
-//     return a-b;
+// let Astro: dog = {
+//     name: "Astro",
+//     age: 5
 // }
-// const logDetails = (uid: string | number, item: string) => {
-//     console.log(`${item} has a uid of ${uid}`)
-// }
-// type StringOrNum = string | number; this can replace the uid type
-var greet;
+// logDetails(Astro);
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,74 @@
-let greet: (a: string, b: string) => void;
+// classes
 
-console.log("hello");
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes \$${this.amount} for ${this.details}`;
+    }
+}
+
+let invoices: Invoice[] = [];
+
+// const invOne = new Invoice("Yoshi", "plumbing", 250);
+
+// console.log(invOne.format());
+
+// Dealing with forms and inputs with TypeScript
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector('#type')! as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom')! as HTMLInputElement;
+const details = document.querySelector('#details')! as HTMLInputElement;
+const amount = document.querySelector('#amount')! as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    const invoice = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+
+    console.log(invoice.format());
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TypeCasting
+
+// let logDetails: (obj: {name: string, age: number}) => void;
+
+// type dog = {name: string, age: number};
+
+// logDetails = (goodBoy: dog) => {
+//     console.log(`His name is: ${goodBoy.name} and he is ${goodBoy.age} years old.`);
+// }
+
+// let Astro: dog = {
+//     name: "Astro",
+//     age: 5
+// }
+
+// logDetails(Astro);
